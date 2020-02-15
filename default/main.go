a2bd001a0560a57950cc06ea18db3b9c18adf3fe
Kai-Chu Chung <cage.chung@gmail.com>
2020-02-15 12:21:49 +0800

    feat(service): add ng-web as default service

---
+++ default/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.Handle("/", http.FileServer(http.Dir("ng-web/dist/ng-web")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
